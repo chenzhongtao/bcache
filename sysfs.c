b43e0c14435dae6b8bc5dcda3af7a5308c63d1ec
zhang_chaolu <zhang_chaolu@topsec.com.cn>
2019-03-13 19:37:40 +0800

    make the pr_err statement used for ENOENT only in sysfs_attatch section

--- sysfs.c
+++ sysfs.c
@@ -366,7 +366,8 @@ STORE(__cached_dev)
 				return size;
 		}
 
-		pr_err("Can't attach %s: cache set not found", buf);
+		if (v == -ENOENT)
+			pr_err("Can't attach %s: cache set not found", buf);
 		return v;
 	}
 
