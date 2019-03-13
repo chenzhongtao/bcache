1265ec4aaf3846d5b62cbdf22a6ecb01fa3babaf
zhang_chaolu <zhang_chaolu@topsec.com.cn>
2019-03-13 19:03:35 +0800

    fix compile error debug

--- debug.c
+++ debug.c
@@ -248,7 +248,7 @@ void bch_debug_exit(void)
 		debugfs_remove_recursive(bcache_debug);
 }
 
-int __init bch_debug_init(struct kobject *kobj)
+void __init bch_debug_init(struct kobject *kobj)
 {
     /*
  	 * it is unnecessary to check return value of
